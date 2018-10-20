d700639c6c444dac7957360632eb5fee0c79360e
Sigtrap <contact@sigtrapgames.com>
2018-10-20 20:48:28 +0100

    Example 3

---
+++ Examples/Common/Scripts/SmartDataExample_OnHit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SmartData.Examples {
+	[RequireComponent(typeof(Collider))]
+	public class SmartDataExample_OnHit : MonoBehaviour {
+		public string nameMatch;
+		public SmartEvent.EventDispatcher onHit;
+		
+		void OnCollisionEnter(Collision col){
+			if (col.collider.name == nameMatch){
+				onHit.Dispatch();
+			}
+		}
+	}
+}
